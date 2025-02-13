7cdd0535e22c29333bc6cef0a10c8386b5800dba
underpostnet <fcoverdugoa@underpost.net>
2025-02-13 18:03:52 +0000

    ci(engine-cyberia-repo-build): ⚙️ Update engine cyberia repository

--- src/api/instance/instance.service.js
+++ src/api/instance/instance.service.js
@@ -7,15 +7,15 @@ const logger = loggerFactory(import.meta);
 const InstanceService = {
   post: async (req, res, options) => {
     /** @type {import('./instance.model.js').InstanceModel} */
-    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.models.Instance;
+    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.Instance;
     return await new Instance(req.body).save();
   },
   get: async (req, res, options) => {
     /** @type {import('./instance.model.js').InstanceModel} */
-    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.models.Instance;
+    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.Instance;
 
     /** @type {import('../user/user.model.js').UserModel} */
-    const User = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.models.User;
+    const User = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.User;
 
     const user = await User.findOne({
       _id: req.auth.user._id,
@@ -34,12 +34,12 @@ const InstanceService = {
   },
   put: async (req, res, options) => {
     /** @type {import('./instance.model.js').InstanceModel} */
-    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.models.Instance;
+    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.Instance;
     return await Instance.findByIdAndUpdate(req.params.id, req.body);
   },
   delete: async (req, res, options) => {
     /** @type {import('./instance.model.js').InstanceModel} */
-    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.models.Instance;
+    const Instance = DataBaseProvider.instance[`${options.host}${options.path}`].mongoose.Instance;
     if (req.params.id) return await Instance.findByIdAndDelete(req.params.id);
     else return await await Instance.deleteMany();
   },

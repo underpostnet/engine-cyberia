48c34340dd59658c8c520099ea3089def426366d
underpostnet <development@underpost.net>
2025-11-07 20:39:15 +0000

    ci(engine-cyberia-repo-build): ⚙️ Update engine cyberia repository

--- bin/cyberia.js
+++ bin/cyberia.js
@@ -10,6 +10,7 @@ import {
   pngDirectoryIteratorByObjectLayerType,
   frameFactory,
   getKeyFramesDirectionsFromNumberFolderDirection,
+  processAndPushFrame,
   buildImgFromTile,
   generateRandomStats,
   itemTypes,
@@ -35,11 +36,23 @@ program
   .version(version);
 
 program
+  .command('underpost')
+  .description('Underpost cli passthrough')
+  .action(() => {
+    process.argv = process.argv.filter((c) => c !== 'underpost');
+    underpostProgram.parse();
+  });
+
+program
   .command('ol')
   .option('--import [object-layer-type]', 'Commas separated object layer types e.g. skin,floors')
   .option('--show-frame <show-frame-input>', 'View object layer frame e.g. anon_08_0')
   .option('--env-path <env-path>', 'Env path e.g. ./engine-private/conf/dd-cyberia/.env.development')
-  .action(async (options = { import: false, showFrame: '', envPath: '' }) => {
+  .option('--mongo-host <mongo-host>', 'Mongo host override')
+  .action(async (options = { import: false, showFrame: '', envPath: '', mongoHost: '' }) => {
+    if (!options.envPath) options.envPath = `./env`;
+    dotenv.config({ path: options.envPath, override: true });
+
     const deployId = process.env.DEFAULT_DEPLOY_ID;
     const host = process.env.DEFAULT_DEPLOY_HOST;
     const path = process.env.DEFAULT_DEPLOY_PATH;
@@ -48,7 +61,7 @@ program
     const confServer = JSON.parse(fs.readFileSync(confServerPath, 'utf8'));
     const { db } = confServer[host][path];
 
-    db.host = db.host.replace('127.0.0.1', 'mongodb-0.mongodb-service');
+    db.host = options.mongoHost ? options.mongoHost : db.host.replace('127.0.0.1', 'mongodb-0.mongodb-service');
 
     logger.info('env', {
       deployId,
@@ -67,9 +80,6 @@ program
     const ObjectLayer = DataBaseProvider.instance[`${host}${path}`].mongoose.models.ObjectLayer;
 
     await ObjectLayer.deleteMany();
-    if (!options.envPath) options.envPath = `./engine-private/conf/dd-cyberia/.env.production`;
-
-    dotenv.config({ path: options.envPath, override: true });
 
     const objectLayers = {};
 
@@ -101,16 +111,8 @@ program
                       stats: generateRandomStats(),
                     },
                   };
-              objectLayers[objectLayerId].data.render.colors = [];
-              objectLayers[objectLayerId].data.render.frames = {};
             }
-            const frameFactoryResult = await frameFactory(path, objectLayers[objectLayerId].data.render.colors);
-            objectLayers[objectLayerId].data.render.colors = frameFactoryResult.colors;
-            for (const objectLayerFrameDirection of getKeyFramesDirectionsFromNumberFolderDirection(direction)) {
-              if (!objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection])
-                objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection] = [];
-              objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection].push(frameFactoryResult.frame);
-            }
+            await processAndPushFrame(objectLayers[objectLayerId].data.render, path, direction);
           },
         );
       }
@@ -143,8 +145,8 @@ program
   .description('Object layer management');
 
 try {
+  // throw new Error('');
   program.parse();
 } catch (error) {
-  console.error('Cyberia cli reference not found', error);
-  underpostProgram.parse();
+  logger.error(error);
 }
--- src/client/services/object-layer/object-layer.management.js
+++ src/client/services/object-layer/object-layer.management.js
@@ -1,20 +1,113 @@
 import { DefaultManagement } from '../default/default.management.js';
 import { ObjectLayerService } from './object-layer.service.js';
 import { commonUserGuard } from '../../components/core/CommonJs.js';
+import { getProxyPath, setPath } from '../../components/core/Router.js';
+import { ObjectLayerEngineModal } from '../../components/core/ObjectLayerEngineModal.js';
+import { s } from '../../components/core/VanillaJs.js';
+import { Modal } from '../../components/core/Modal.js';
+import { BtnIcon } from '../../components/core/BtnIcon.js';
 
 const ObjectLayerManagement = {
   RenderTable: async ({ Elements }) => {
     const user = Elements.Data.user.main.model.user;
     const { role } = user;
-    let columnDefs = [{ field: 'data.item.id', headerName: 'itemId', editable: role === 'user' }];
-    switch (role) {
-      case 'admin':
-        {
-        }
-        break;
 
-      default:
-        break;
+    // Custom renderer for edit button
+    class EditButtonRenderer {
+      eGui;
+
+      async init(params) {
+        this.eGui = document.createElement('div');
+        const { data } = params;
+
+        if (!data || !data._id) {
+          this.eGui.innerHTML = '';
+          return;
+        }
+
+        this.eGui.innerHTML = html` ${await BtnIcon.Render({
+          label: html`<div class="abs center">
+            <i class="fas fa-edit"></i>
+          </div> `,
+          class: `in fll section-mp management-table-btn-mini btn-edit-object-layer-${data._id}`,
+        })}`;
+
+        setTimeout(() => {
+          s(`.btn-edit-object-layer-${data._id}`).onclick = async () => {
+            if (s(`.modal-object-layer-engine`)) await ObjectLayerEngineModal.Reload();
+            else s(`.main-btn-object-layer-engine`).click();
+            setTimeout(() => {
+              setPath(`${getProxyPath()}?cid=${data._id}`);
+            });
+          };
+        });
+      }
+
+      getGui() {
+        return this.eGui;
+      }
+
+      refresh(params) {
+        return true;
+      }
+    }
+
+    // Custom renderer for the frame 08 preview
+    class Frame08Renderer {
+      eGui;
+
+      async init(params) {
+        this.eGui = document.createElement('div');
+        const { data } = params;
+
+        if (!data || !data.data || !data.data.item) {
+          this.eGui.innerHTML = '';
+          return;
+        }
+
+        const { type, id } = data.data.item;
+        const imagePath = `${getProxyPath()}assets/${type}/${id}/08/0.png`;
+
+        this.eGui.innerHTML = html`
+          <img class="inl" src="${imagePath}" style="width: 100px; height: 100px;" alt="Frame 08" />
+        `;
+      }
+
+      getGui() {
+        return this.eGui;
+      }
+
+      refresh(params) {
+        return true;
+      }
+    }
+
+    let columnDefs = [
+      { field: '_id', headerName: 'Storage ID', width: 220, editable: false },
+      { field: 'data.item.id', headerName: 'Item ID', editable: role === 'user' },
+      { field: 'data.item.type', headerName: 'Item Type', editable: role === 'user' },
+      { field: 'data.item.description', headerName: 'Description', flex: 1, editable: role === 'user' },
+      {
+        field: 'frame08',
+        headerName: 'Frame 08 Preview',
+        width: 120,
+        cellRenderer: Frame08Renderer,
+        editable: false,
+        sortable: false,
+        filter: false,
+      },
+    ];
+
+    if (commonUserGuard(role)) {
+      columnDefs.push({
+        field: 'edit',
+        headerName: '',
+        width: 100,
+        cellRenderer: EditButtonRenderer,
+        editable: false,
+        sortable: false,
+        filter: false,
+      });
     }
     return await DefaultManagement.RenderTable({
       idModal: 'modal-object-layer-engine-management',
@@ -23,6 +116,7 @@ const ObjectLayerManagement = {
       permissions: {
         add: commonUserGuard(role),
         remove: commonUserGuard(role),
+        reload: commonUserGuard(role),
       },
       columnDefs,
       customFormat: (obj) => {
@@ -37,6 +131,14 @@ const ObjectLayerManagement = {
       defaultColKeyFocus: 'data.item.id',
       ServiceProvider: ObjectLayerService,
     });
+  },
+  Reload: async function () {
+    const idModal = 'modal-object-layer-engine-management';
+    if (s(`.modal-object-layer-engine-management`))
+      Modal.writeHTML({
+        idModal,
+        html: await Modal.Data[idModal].options.html(),
+      });
   },
 };
 

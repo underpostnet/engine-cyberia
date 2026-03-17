13f60cee007ec1678709094673a81d52ae5e6a3d
underpostnet <development@underpost.net>
2026-03-17 21:55:00 +0000

    2026-03-16   cli-client  feat: Add sync-conf command and enhance build options for client assets   swagger  fix: update security middleware to ensure correct CSP headers for Swagger UI   scripts  Add missing package installation command in ports-ls.sh   New release v:3.1.2   cli-run  Fix streamline deployment logic prevent override env files and redundant build workflows   engine-cyberia  fix: update error handling for underpost passthrough to include 'env' command   New release v:3.1.1   cli-index  fix: add quiet option to dotenv config for improved error handling   cli-secrets  fix: add missing dotenv import in secrets module   cli-repository  fix: enhance environment variable checks for GITHUB_TOKEN retrieval   New release v:3.1.0   docs  Add documentation for Baremetal and LXD management, including command references and quick start guides   github-actions  feat: add optional commit message input for workflow_dispatch in CI configurations   cli-repository  feat: enhance CI/CD workflows with workflow_dispatch support and input options   package  fix: add production image script to package.json

--- conf.js
+++ conf.js
@@ -606,6 +606,11 @@ const DefaultConf = /**/ {
         ],
         ws: 'core',
         peer: true,
+        docs: {
+          coveragePath: './hardhat',
+          jsJsonPath: './jsdoc.dd-cyberia.json',
+          references: ['./hardhat/README.md', './hardhat/WHITE-PAPER.md'],
+        },
         origins: [
           'https://www.cyberiaonline.com',
           'https://server.cyberiaonline.com',
@@ -644,6 +649,11 @@ const DefaultConf = /**/ {
         ],
         ws: 'core',
         peer: true,
+        docs: {
+          coveragePath: './hardhat',
+          jsJsonPath: './jsdoc.dd-cyberia.json',
+          references: ['./hardhat/README.md', './hardhat/WHITE-PAPER.md'],
+        },
         origins: [
           'https://www.cyberiaonline.com',
           'https://server.cyberiaonline.com',
